2e93442a5a47920b9fc0bd3d5f15b3c52daa3fe5
Николай <kirpix524@gmail.com>
2025-07-30 15:51:44 +0400

    implemented user login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import RegisterView
+from django.urls import path
+from .views import RegisterView, ProfileView
 
 app_name = 'users'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
         template_name='users/login.html'
     ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
