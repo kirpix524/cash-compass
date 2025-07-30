7d5a2dbab48180b00d599989e0cc986cbba7003d
Николай <kirpix524@gmail.com>
2025-07-30 15:39:40 +0400

    implemented user registration

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import RegisterView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html'
+    ), name='login'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    template_name = "users/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("users:login")
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name: str = "users/profile.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context: Dict[str, Any] = super().get_context_data(**kwargs)
+        context["user"] = self.request.user
+        return context
